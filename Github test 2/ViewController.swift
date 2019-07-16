75a4ea5a4ba4afa2deb88766fe0036ed1f326932
GWC <gwc@cs81-us-mus-bb.local>
2019-07-16 13:04:05 -0400

    Initial Commit

---
+++ Github test 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github test 2
+//
+//  Created by GWC on 7/16/19.
+//  Copyright © 2019 GWC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
